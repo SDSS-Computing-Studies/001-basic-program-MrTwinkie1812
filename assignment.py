6b4cc7e4136331d65501565704679fa7af2098cd
MrTwinkie1812 <94935761+MrTwinkie1812@users.noreply.github.com>
2022-01-14 09:57:13 -0800

    update

--- assignment.py
+++ assignment.py
@@ -1,3 +1,13 @@
 #! python3
 
 # SD Computing Studies Assignment
+"""
+(2 points) Modify the existing assignment file called "assignment.py".  It should display the message "Hello World!" Note that the output is case sensitive. Make sure that it is an exact match
+
+Sample Output Task #1
+Hello World!
+"""
+print ("\"Hello World!\"")
+# Create a repository and edit the files inside it
+#* Make use of the print() function to generate output
+#* Make use of escaped characters: \n \" \' to print line breaks or quotation marks
